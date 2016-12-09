9b90160d99b0d1b3d78a134c24f723a814f7214a
Gustavo Monteiro <gusrmonteiro@live.com>
2016-12-09 20:58:04 -0200

    acertando o do.sh

--- do.sh
+++ do.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
+
 if [ $# -ge 2 ]; then
+
+oredem_da_matriz=$1
+interacoes=$2
+flag1=$3
+flag2=$4
 
 nvcc gpu_difusao_tabelas.cu -o otim
 
-./otim $1 $1 $2  $3
+./otim $oredem_da_matriz $oredem_da_matriz $interacoes $flag1 $flag2
 
 	if [ $# -lt 3 ]; then
 
 	 gcc diferenca.c -o compara
-
-	 ./compara out_gpu.txt out_cpu.txt 32
+	 colunas=$(($oredem_da_matriz + 2))
+	 ./compara out_gpu.txt out_cpu.txt $colunas
 
 	fi
 else  
